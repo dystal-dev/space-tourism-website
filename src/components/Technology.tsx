49ef20d2d78bb757dc46783022be973277ee4bac
dystal-dev <jeremyhabal@yahoo.com>
2026-04-09 09:41:14 +0800

    feat(technology): add tab switching for technology selection

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,6 +1,11 @@
-import ImageLaunchVehicleProject from "../assets/technology/image-launch-vehicle-portrait.jpg";
+import { useState } from "react";
+import { technology, type TechnologyItem } from "../data/data";
+import Button from "./ui/Button";
 
 export default function Technology() {
+  const [currentTechnologyItem, setCurrentTechnologyItem] =
+    useState<TechnologyItem>(technology[0]);
+
   return (
     <div className="flex flex-col w-full max-w-[1275px] my-600 gap-300 ml-600 ml-auto h-full">
       <div className="flex gap-300">
@@ -12,31 +17,32 @@ export default function Technology() {
       <div className="flex flex-row items-center justify-center h-full gap-500">
         <div className="flex w-full gap-800 min-w-[635px] items-center">
           <div className="flex flex-col items-center justify-center gap-400">
-            <div className="flex items-center justify-center text-preset-4 text-blue-900 h-1000 w-1000 rounded-full bg-white">
-              1
-            </div>
-            <div className="flex items-center justify-center text-preset-4 text-white h-1000 w-1000 rounded-full border-1 border-white/25">
-              2
-            </div>
-            <div className="flex items-center justify-center text-preset-4 text-white h-1000 w-1000 rounded-full border-1 border-white/25">
-              3
-            </div>
+            {technology.map((item, index) => (
+              <Button
+                key={item.name}
+                variant="numberIndicator"
+                className={`
+                  ${item.name === currentTechnologyItem.name ? "bg-white !text-blue-900" : ""}
+                `}
+                onClick={() => setCurrentTechnologyItem(item)}
+              >
+                {index + 1}
+              </Button>
+            ))}
           </div>
           <div className="flex flex-col gap-300">
             <div className="flex flex-col text-preset-8 uppercase gap-200">
-              <div className="text-preset-4 uppercase text-white/50">
-                THE TERMINOLOGY…
+              <div className="text-preset-4 uppercase text-white/50 uppercase">
+                the terminology...
               </div>
-              <div className="text-preset-3 uppercase">LAUNCH VEHICLE</div>
+              <div className="text-preset-3 uppercase">
+                {currentTechnologyItem.name}
+              </div>
             </div>
 
             <div className="flex flex-col gap-200">
               <div className="text-preset-9 text-blue-300">
-                A launch vehicle or carrier rocket is a rocket-propelled vehicle
-                used to carry a payload from Earth's surface to space, usually
-                to Earth orbit or beyond. Our WEB-X carrier rocket is the most
-                powerful in operation. Standing 150 metres tall, it's quite an
-                awe-inspiring sight on the launch pad!
+                {currentTechnologyItem.description}
               </div>
             </div>
           </div>
@@ -44,8 +50,8 @@ export default function Technology() {
 
         <div className="flex w-full gap-500 items-center">
           <img
-            src={ImageLaunchVehicleProject}
-            alt="Launch Vehicle"
+            src={currentTechnologyItem.images.portrait}
+            alt={currentTechnologyItem.name}
             className="w-full h-full object-cover"
           />
         </div>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,6 +4,8 @@ const buttonVariants = {
   primary:
     "inline-flex text-preset-8 gap-150 border-b-3 uppercase border-white/0 hover:border-white/50",
   dotIndicator: "w-[15px] h-[15px] rounded-full bg-white/17 hover:bg-white/50",
+  numberIndicator:
+    "flex items-center justify-center text-preset-4 text-white h-1000 w-1000 rounded-full border-1 border-white/25 hover:border-white/100",
 } as const;
 
 type ButtonVariant = keyof typeof buttonVariants;
