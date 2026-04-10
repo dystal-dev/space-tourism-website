1a3af68129e73ce532a26005a68b65b19d37211d
dystal-dev <jeremyhabal@yahoo.com>
2026-04-10 23:10:05 +0800

    feat(ui): add destination.tsx responsiveness

--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -8,26 +8,28 @@ export default function Destination() {
   );
 
   return (
-    <div className="flex flex-col w-full max-w-[1110px] gap-300 my-600 mx-auto h-full">
-      <div className="flex gap-300">
-        <div className="text-preset-5 text-white/25 uppercase font-bold">
-          01
-        </div>
-        <div className="text-preset-5 uppercase">PICK YOUR DESTINATION</div>
+    <div className="flex flex-1 flex-col p-300 w-full max-w-[1110px] gap-300 mx-auto md:p-500 lg:my-600 lg:p-0">
+      <div className="flex justify-center gap-300 md:justify-start text-preset-5 lg:text-preset-5-desktop">
+        <div className="text-white/25 uppercase font-bold">01</div>
+        <div className="uppercase">PICK YOUR DESTINATION</div>
       </div>
-      <div className="flex w-full h-full gap-400">
-        <div className="flex w-full h-full items-center justify-center">
-          <img src={currentDestination.image} alt={currentDestination.name} />
+      <div className="flex flex-col flex-1 w-full gap-400 lg:flex-row">
+        <div className="flex flex-1 w-full items-center justify-center basis-0">
+          <img
+            src={currentDestination.image}
+            alt={currentDestination.name}
+            className="h-[150px] w-[150px] md:h-[300px] md:w-[300px] lg:h-[480px] lg:w-[480px]"
+          />
         </div>
-        <div className="flex w-full h-full items-center justify-center min-w-[445px]">
-          <div className="flex flex-col w-full max-w-[445px] gap-500">
-            <div className="flex text-preset-8 gap-400">
+        <div className="flex flex-1 flex-col w-full items-center justify-center basis-0 md:min-w-[445px]">
+          <div className="flex flex-col w-full md:max-w-[514px] lg:max-w-[445px] gap-300 lg:gap-500">
+            <div className="flex justify-center text-preset-8 gap-400 lg:justify-start">
               {destinations.map((item) => (
                 <Button
                   key={item.name}
                   variant="primary"
                   className={`
-                    h-400 text-blue-300 hover:text-white 
+                    h-400 text-blue-300 hover:text-white
                     ${
                       item.name === currentDestination.name
                         ? "!text-white !border-white/100"
@@ -40,19 +42,21 @@ export default function Destination() {
                 </Button>
               ))}
             </div>
-            <div className="flex flex-col gap-200">
-              <div className="text-preset-2 uppercase">
+            <div className="flex flex-col text-center gap-200 lg:text-left">
+              <div className="text-preset-2 uppercase md:text-preset-2-tablet">
                 {currentDestination.name}
               </div>
-              <div className="text-preset-9 text-blue-300">
+              <div className="flex text-preset-9 text-blue-300 md:text-preset-9-tablet lg:text-preset-9-desktop">
                 {currentDestination.description}
               </div>
             </div>
             <hr className="border-white/25" />
-            <div className="flex w-full gap-300 uppercase">
+            <div className="flex flex-col w-full gap-300 uppercase text-center md:flex-row lg:text-left">
               <div className="flex flex-col gap-150 w-full">
-                <div className="text-preset-7 text-blue-300">Avg. Distance</div>
-                <div className="text-preset-6">
+                <div className="text-preset-7 text-preset-7-desktop text-blue-300">
+                  Avg. Distance
+                </div>
+                <div className="text-preset-6 text-preset-6-desktop">
                   {currentDestination.distance}
                 </div>
               </div>
@@ -60,7 +64,9 @@ export default function Destination() {
                 <div className="text-preset-7 text-blue-300">
                   EST. Travel Time
                 </div>
-                <div className="text-preset-6">{currentDestination.travel}</div>
+                <div className="text-preset-6 text-preset-6-desktop">
+                  {currentDestination.travel}
+                </div>
               </div>
             </div>
           </div>
